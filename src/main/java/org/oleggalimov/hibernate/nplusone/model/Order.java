ef2e13d34cbb18a7678e3cb91f2ccb1329ff1525
oleg <o.d.galimov@yandex.ru>
2019-12-01 13:51:02 +0500

    First step - just initialise project, using DataSource connection.

---
+++ src/main/java/org/oleggalimov/hibernate/nplusone/model/Order.java
@@ -0,0 +1,105 @@
+package org.oleggalimov.hibernate.nplusone.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity
+public class Order {
+    @Id
+    private
+    Integer id;
+
+    private String firstName;
+    private String lastName;
+    private Long phoneNumber;
+    private String address;
+    private Integer status;
+    private Timestamp creationDate;
+    private Timestamp sendingDate;
+    private Timestamp recevingDate;
+    private String comments;
+
+    public Order() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Timestamp getSendingDate() {
+        return sendingDate;
+    }
+
+    public void setSendingDate(Timestamp sendingDate) {
+        this.sendingDate = sendingDate;
+    }
+
+    public Timestamp getRecevingDate() {
+        return recevingDate;
+    }
+
+    public void setRecevingDate(Timestamp recevingDate) {
+        this.recevingDate = recevingDate;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+}
